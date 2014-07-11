a8fbf715af83809a6ae045efdacc2b58435c239a
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2014-07-10 21:48:47 -0500

    added mean config

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -1,1 +1,18 @@
-# Add puppet config here
+class {'nodejs':
+	version	=> 'v0.10.28'
+}
+
+package {'grunt-cli':
+	ensure		=> '0.1.13',
+	provider	=> 'npm',
+	require		=> Class['nodejs']
+}
+
+exec {'npm-install':
+	command	=> '/usr/local/node/node-default/bin/npm install',
+	cwd		=> '/vagrant',
+	require	=> Class['nodejs']
+}
+
+
+include '::mongodb::server'
